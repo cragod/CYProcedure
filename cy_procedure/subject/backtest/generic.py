dc5a18da369d3b40aa71ffcbdf3bc58b593dae31
hyl <hyl4@meitu.com>
2020-07-15 10:09:39 +0800

    update bt proc

--- cy_procedure/subject/backtest/generic.py
+++ cy_procedure/subject/backtest/generic.py
@@ -1,39 +1,53 @@
 from datetime import datetime
+from cy_components.helpers.formatter import DateFormatter as dfr
 from cy_components.defines.column_names import *
 from cy_widgets.backtest.strategy import *
 from cy_widgets.backtest.helper import *
+from cy_data_access.models.backtest import *
 from multiprocessing.pool import Pool
 
 
 class SimpleBacktestProcedure:
     """简单回测流程"""
 
-    def __init__(self, df, strategy_cls, params, position_func, evaluation_func):
+    def __init__(self, coin_pair, df, strategy_cls, params, position_func, evaluation_func):
         self.__df = df
         self.__strategy_cls = strategy_cls
         self.__params = params
         self.__position_func = position_func
         self.__evaluation_func = evaluation_func
+        self.__summary_collection_cls = backtest_summary_class(
+            '{}_{}_{}'.format(dfr.convert_local_date_to_string(datetime.now(), '%Y%m%d%H%M%S'), coin_pair.formatted('_').lower(), str(strategy_cls()).lower()))
+
+        self.__start_date = df.iloc[0][COL_CANDLE_BEGIN_TIME]
+        self.__end_date = df.iloc[-1][COL_CANDLE_BEGIN_TIME]
 
     def __result_handler(self, evaluated_df, strategy, error_des):
         if error_des is not None:
             print(error_des)
-            return None
         else:
-            rtn = pd.DataFrame()
-            rtn.loc[0, 'param'] = StrategyHelper.formatted_identifier(strategy)
-            rtn.loc[0, 'equity_curve'] = evaluated_df.iloc[-1][COL_EQUITY_CURVE]
-            return rtn
+            param = StrategyHelper.formatted_identifier(strategy)
+            curve = str(evaluated_df.iloc[-1][COL_EQUITY_CURVE])
+            print('finished', param, curve)
+            self.__summary_collection_cls(
+                parameter=param,
+                equity_curve=curve,
+                start_date=self.__start_date,
+                end_date=self.__end_date
+            ).save()
 
     def calculation(self, param):
-        bt = StrategyBacktest(self.__df.copy(), self.__strategy_cls(
-            **param), self.__position_func, self.__evaluation_func, self.__result_handler)
-        return bt.perform_test()
+        try:
+            bt = StrategyBacktest(self.__df.copy(), self.__strategy_cls(
+                **param), self.__position_func, self.__evaluation_func, self.__result_handler)
+            return bt.perform_test()
+        except Exception as e:
+            print(str(param), str(e))
+            return None
 
     def perform_test_proc(self):
+        # self.calculation(self.__params[0])
         with Pool(processes=2) as pool:
             start_date = datetime.now()
-            result_dfs = pool.map(self.calculation, self.__params)
-            totally_dfs = pd.concat(result_dfs, ignore_index=True)
+            pool.map(self.calculation, self.__params)
             print(datetime.now() - start_date)
-            print(totally_dfs)
