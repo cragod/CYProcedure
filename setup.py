50d3e8b4b86b8dfb658a52fb19161302e12b6ede
hyl <hyl4@meitu.com>
2020-07-17 10:23:25 +0800

    Bump version: 0.2.8 → 0.2.9

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cragod/CYProcedure',
-    version='0.2.8',
+    version='0.2.9',
     zip_safe=False,
 )
