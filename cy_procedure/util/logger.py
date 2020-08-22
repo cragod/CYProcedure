e2d5c23c6d07c8f77784109bc2ab505dc0cc57f7
hyl <hyl4@meitu.com>
2020-08-22 21:42:35 +0800

    update recorder

--- cy_procedure/util/logger.py
+++ cy_procedure/util/logger.py
@@ -9,7 +9,6 @@ class ProcedureRecorder(RecorderBase):
     def __init__(self, m_token: str, m_type: MessageType):
         self.__message_token = m_token
         self.__message_type = m_type
-        self.__summary_log = ""
 
     def record_simple_info(self, content):
         print(content, end='\n\n')
@@ -22,14 +21,5 @@ class ProcedureRecorder(RecorderBase):
         print(content, end='\n\n')
         MessageHandler.send_message(content, 'Precedure Exception', self.__message_type, self.__message_token)
 
-    def append_summary_log(self, content):
-        # 打印并加入到最终日志
-        # date_str = DateFormatter.convert_local_date_to_string(datetime.now(), '%H:%M:%S')
-        self.record_simple_info(content)
-        self.__summary_log = self.__summary_log + content + '\n\n'
-
-    def record_summary_log(self, content=None):
-        if content is not None:
-            self.append_summary_log(content)
-        MessageHandler.send_message(self.__summary_log, 'Procedure Summary', self.__message_type, self.__message_token)
-        self.__summary_log = ""
+    def _record_summary_log(self):
+        MessageHandler.send_message(self._summary_log, 'Procedure Summary', self.__message_type, self.__message_token)
