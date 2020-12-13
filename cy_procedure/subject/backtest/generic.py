e2ca32b241e36fb40474ecb605de2c55708850a9
黄云龙 <hyl4@meitu.com>
2020-12-14 00:13:53 +0800

    update backtest

--- cy_procedure/subject/backtest/generic.py
+++ cy_procedure/subject/backtest/generic.py
@@ -26,18 +26,17 @@ class GenericBacktestProcedure:
         self.__evaluation_func = evaluation_func
         self.__task_identifier = task_identifier
 
-    def __result_handler(self, context, position_df, evaluated_df, strategy, error_des):
+    def __result_handler(self, context, position_df, evaluated_res, strategy, error_des):
         """单次回测结束"""
         if error_des is not None:
             print('回测失败', error_des)
         else:
             param_identifier = context['param_identifier']
-            curve = str(evaluated_df.iloc[-1][COL_EQUITY_CURVE])
             # 简要数据
-            print(position_df.shape)
             overview = BacktestOverview(task_identifier=self.__task_identifier,
                                         param_identifier=param_identifier,
-                                        equity_curve=curve)
+                                        equity_curve=evaluated_res['累积净值'],
+                                        statics_info=evaluated_res)
             json_list = convert_df_to_json_list(position_df, COL_CANDLE_BEGIN_TIME)
             # 保存信号表
             backtest_signal_candle_class(overview.signal_collection_name()).bulk_upsert_records(json_list)
@@ -46,27 +45,27 @@ class GenericBacktestProcedure:
 
     def calculation(self, param):
         """单次回测开始"""
-        try:
-            strategy = self.__strategy_cls(**param)
-            param_str = "|".join(["{}:{}".format(key, param[key]) for key in sorted(param.keys())])
-            start_date = dfr.convert_local_date_to_string(self.__df.iloc[0][COL_CANDLE_BEGIN_TIME], "%Y%m%d")
-            end_date = dfr.convert_local_date_to_string(self.__df.iloc[-1][COL_CANDLE_BEGIN_TIME], "%Y%m%d")
-            param_identifier = "{}|{}|{}|{}".format(strategy.name, param_str, self.__time_frame, "{},{}".format(start_date, end_date))
+        # try:
+        strategy = self.__strategy_cls(**param)
+        param_str = "|".join(["{}:{}".format(key, param[key]) for key in sorted(param.keys())])
+        start_date = dfr.convert_local_date_to_string(self.__df.iloc[0][COL_CANDLE_BEGIN_TIME], "%Y%m%d")
+        end_date = dfr.convert_local_date_to_string(self.__df.iloc[-1][COL_CANDLE_BEGIN_TIME], "%Y%m%d")
+        param_identifier = "{}|{}|{}|{}".format(strategy.name, param_str, self.__time_frame, "{},{}".format(start_date, end_date))
 
-            if len(list(BacktestOverview.objects.raw({'task_identifier': self.__task_identifier,
-                                                      'param_identifier': param_identifier}))) > 0:
-                print("{} 已存在，跳过".format(param_identifier))
-                return None
-
-            context = {
-                'param_identifier': param_identifier
-            }
-
-            bt = StrategyBacktest(self.__df.copy(), strategy, self.__position_func, self.__evaluation_func, self.__result_handler, context)
-            return bt.perform_test()
-        except Exception as e:
-            print(str(param), str(e))
+        if len(list(BacktestOverview.objects.raw({'task_identifier': self.__task_identifier,
+                                                  'param_identifier': param_identifier}))) > 0:
+            print("{} 已存在，跳过".format(param_identifier))
             return None
+
+        context = {
+            'param_identifier': param_identifier
+        }
+
+        bt = StrategyBacktest(self.__df.copy(), strategy, self.__position_func, self.__evaluation_func, self.__result_handler, context)
+        return bt.perform_test()
+        # except Exception as e:
+        #     print(str(param), str(e))
+        #     return None
 
     def perform_test_proc(self, processes=2):
         for time_frame in self.__raw_time_frames:
