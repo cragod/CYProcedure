bc030058c4bf82827b50241e00f59913a889a971
Talon.Huang <talon.huang@ringcentral.com>
2023-10-08 00:41:08 +0800

    Bump version: 0.5.49 → 0.5.50

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cragod/CYProcedure',
-    version='0.5.49',
+    version='0.5.50',
     zip_safe=False,
 )
